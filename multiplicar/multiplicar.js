28cfc263947e999532ab7273a169fc3ec06f6fa0
Carlos <carlosmontoya@outlook.com>
2018-09-03 15:03:58 -0500

    commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,63 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un numero`);
+            return;
+        }
+        if (!Number(limite)) {
+            reject(`El valor introducido ${ limite } no es un numero`);
+            return;
+        }
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data = `${ base } * ${ i } = ${ base*i }\n`;
+            console.log(data);
+        }
+
+
+    });
+
+
+
+}
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+        if (!Number(base)) {
+            reject(`El valor introducido ${ base } no es un numero`);
+            return;
+        }
+        if (!Number(limite)) {
+            reject(`El valor introducido ${ limite } no es un numero`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base*i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, (err) => {
+            if (err)
+                reject(err)
+            else
+                resolve(`tabla-${ base }-al-${ limite }.txt`.green)
+
+        });
+
+    });
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
